8d47d47b6dc5880056532299a50f024099d1b862
SamEThibault <sam5thibault@gmail.com>
2025-01-24 00:18:54 -0500

    greedy notes added

---
+++ Coding/greedy.py
@@ -0,0 +1,6 @@
+# Greedy Algorithms Basics
+# Best seen through a comparison of DP and Greedy approaches:
+
+# Example: rearrange a set of digits to make the largest number
+# DP: Try all of the combinations, then we can pick the best result after
+# Greedy: start with the best option we have for the first digit, then go from there
