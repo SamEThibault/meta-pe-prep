7d577fbf9e2af3b2a8bf5445029fb140d650edf5
SamEThibault <sam5thibault@gmail.com>
2025-01-25 19:27:28 -0500

    hash table added

---
+++ Coding/DataStructures/hash_table.py
@@ -0,0 +1,24 @@
+# Hash table with chaining
+
+class Node:
+    def __init__(self, key, val, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+class HashTable:
+    def __init__(self, capacity):
+        self.store = [None] * capacity
+    
+    def insert(self, key, val):
+        return
+
+    def delete(self, key, val):
+        return
+
+    def _hash(self, key):
+        return
+
+
+
+    
