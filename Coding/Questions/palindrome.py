1d955b11767594f78abf2ba7386cb24be36aab6b
SamEThibault <sam5thibault@gmail.com>
2025-02-08 19:11:03 -0500

    reverse elements added

--- Coding/Questions/palindrome.py
+++ Coding/Questions/palindrome.py
@@ -1,3 +1,20 @@
 """
 Write an algorithm to determine whether a string is a palindrome
 """
+
+def palindrome(string):
+    # What I would usually do:
+    # return string[::-1] == string
+
+    # But since I'm assuming they want double-indexed answer
+    i = 0
+    j = len(string) - 1
+    while i < j:
+        if string[i] != string[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+
+print(palindrome("pelelelelelep"))
--- Coding/Questions/reverse_elements.py
+++ Coding/Questions/reverse_elements.py
@@ -2,3 +2,20 @@
 Given a vector/ array and 2 numbers, eg 2 and 5. 
 Between those 2 positions in the vector (2 and 5) reverse the order of the elements
 """
+
+def partial_reverse(array, start, end):
+    if start < 0 or end > len(array) - 1:
+        return False
+
+    # we're in range, let's do it
+    while start <= end:
+        temp = array[start]
+        array[start] = array[end]
+        array[end] = temp
+        start += 1
+        end -= 1
+    return True
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+partial_reverse(array, 1, 8)
+print(array)
