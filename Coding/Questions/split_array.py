5ea8cd677169d251b1bd71fedc963500fc61a843
SamEThibault <sam5thibault@gmail.com>
2025-02-08 22:47:30 -0500

    split array implemented

--- Coding/Questions/split_array.py
+++ Coding/Questions/split_array.py
@@ -2,36 +2,16 @@
     Given an array of integers greater than zero, find if it is possible to split it in two (without reordering the elements)
     such that the sum of the two resulting arrays is the same. Print the resulting arrays
 """
-
 def split(array):
+    left_sum = 0
+    right_sum = sum(array)
 
-    i = len(array) // 2 # start at mid point
-    left_sum = sum(array[:i])
-    right_sum = sum(array[i:])
-    visited = {}
+    for i in range(len(array) - 1):
+        left_sum += array[i]
+        right_sum -= array[i]
+        if left_sum == right_sum:
+            print(array[:i+1], array[i+1:])
+    
+    return False
 
-    while 0 <= i < len(array) - 1:
-        print(visited)
-        print(i)
-        print(left_sum)
-        print(right_sum)
-        print("###################")
-        if visited.get(i, 0):
-            return False
-        if left_sum < right_sum:
-            left_sum += array[i]
-            right_sum -= array[i]
-            visited[i] = 1
-            i += 1
-        elif left_sum > right_sum:
-            right_sum += array[i]
-            left_sum -= array[i]
-            visited[i] = 1
-            i -= 1
-        else:
-            left = array[:i]
-            right = array[i:]
-            print(left, right)
-            return True
-
-split([4, 4, 8])
+split([1, 2, 3, 4, 5, 15])
