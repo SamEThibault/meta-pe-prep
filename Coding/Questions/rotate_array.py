28cfbd3757d8ae9e4e3cce9c737fde5b632b80e9
SamEThibault <sam5thibault@gmail.com>
2025-02-08 22:40:56 -0500

    rotate array added

--- Coding/Questions/rotate_array.py
+++ Coding/Questions/rotate_array.py
@@ -1,3 +1,18 @@
 """
 In a given array, rotate a part of the array for which the index is given
 """
+
+# Assumptions: give the index for the last element that should be rotated, rotate by 1 clockwise
+def rotate(array, index):
+    if index >= len(array):
+        return False
+
+    og = array[:index + 1]
+    
+    for i in range(index + 1):
+        array[(i + 1) % (index + 1)] = og[i]
+
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+rotate(array, 3)
+print(array)
--- Coding/Questions/split_array.py
+++ Coding/Questions/split_array.py
@@ -3,3 +3,35 @@
     such that the sum of the two resulting arrays is the same. Print the resulting arrays
 """
 
+def split(array):
+
+    i = len(array) // 2 # start at mid point
+    left_sum = sum(array[:i])
+    right_sum = sum(array[i:])
+    visited = {}
+
+    while 0 <= i < len(array) - 1:
+        print(visited)
+        print(i)
+        print(left_sum)
+        print(right_sum)
+        print("###################")
+        if visited.get(i, 0):
+            return False
+        if left_sum < right_sum:
+            left_sum += array[i]
+            right_sum -= array[i]
+            visited[i] = 1
+            i += 1
+        elif left_sum > right_sum:
+            right_sum += array[i]
+            left_sum -= array[i]
+            visited[i] = 1
+            i -= 1
+        else:
+            left = array[:i]
+            right = array[i:]
+            print(left, right)
+            return True
+
+split([4, 4, 8])
